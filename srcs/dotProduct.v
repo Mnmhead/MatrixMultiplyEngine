d3c3c12eaef2d0814b41046eee69bdea4f308048
Wyatt <gyorgym@cs.washington.edu>
2017-03-05 16:53:30 -0800

    OK! The first test of DotProduct worked, now time to try to test pipelined input

--- srcs/dotProduct.v
+++ srcs/dotProduct.v
@@ -26,41 +26,43 @@ module dotProduct #(
     DotProduct 
 );                 
 
-    localparam A_WIDTH = DIM*A_DATA_WIDTH;
-    localparam B_WIDTH = DIM*B_DATA_WIDTH;
-    localparam MULT_EL_WIDTH = A_DATA_WIDTH + B_DATA_WIDTH;
+    // Helpful 'local constants'
     localparam EXTRA_ADD_WIDTH = `CLOG2(DIM);
     localparam RES_WIDTH = A_DATA_WIDTH + B_DATA_WIDTH + EXTRA_ADD_WIDTH; 
-    localparam MULT_WIDTH = A_WIDTH + B_WIDTH;  // width of the vector to hold intermediate multiplication of elements
+    localparam MULT_VECTOR_WIDTH = DIM*(A_DATA_WIDTH + B_DATA_WIDTH);  // width of the vector to hold intermediate multiplication of elements
     
+    
+    // Inputs
     input Clock;
+    input [DIM*A_DATA_WIDTH-1:0] A;  // vector A
+    input [DIM*B_DATA_WIDTH-1:0] B;  // vector B
+
     
-    // A and B are input vectors
-    input [A_WIDTH-1:0] A;
-    input [B_WIDTH-1:0] B;
-    
-    // C is the scalar output
-    output [RES_WIDTH-1:0] DotProduct;
+    // Outputs
+    output [RES_WIDTH-1:0] DotProduct;  // The scalar output
+
 
     // Intermediate reg to hold the products of a_i * b_i, where i is an index from 1 to DIM.
-    wire [MULT_WIDTH-1:0] vector_mult_out;
-    reg [MULT_WIDTH-1:0] vector_mult;
+    wire [MULT_VECTOR_WIDTH-1:0] vector_mult_out;
+    reg [MULT_VECTOR_WIDTH-1:0] vector_mult;
     vectorMult #(DIM, A_DATA_WIDTH, B_DATA_WIDTH) vm( .Clock(Clock), .u(A), .v(B), .result(vector_mult_out) );
 
     always @(posedge Clock) begin
         vector_mult <= vector_mult_out;
     end    
 
-    // Instantiate a vectorSum module to sum all elements of the vector of element-wise products (mult).
+
+    // Instantiate a vectorSum module to sum all elements of the result of vectorMult.
     wire [RES_WIDTH-1:0] vector_sum_out;
     reg [RES_WIDTH-1:0] vector_sum;
     wire readEn;
-    vectorSum #(DIM, MULT_EL_WIDTH) vs( .Clock(Clock), .u(mult), .sum(vector_sum_out), .readEn(readEn) );
+    vectorSum #(DIM, A_DATA_WIDTH + B_DATA_WIDTH) vs( .Clock(Clock), .u(vector_mult), .sum(vector_sum_out), .readEn(readEn) );
     
     always @(posedge Clock) begin
         vector_sum <= vector_sum_out;
     end
     
-    // assign output
+    
+    // Assign Output
     assign DotProduct = vector_sum;
 endmodule
